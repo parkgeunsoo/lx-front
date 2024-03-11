24b8fc58445b31db0867c32c956eb723fadfe0b2
park <rmstn133@gmail.com>
2024-03-11 23:53:33 +0900

    push

---
+++ src/assets/js/ui.js
@@ -0,0 +1,19 @@
+var Header = {
+  init: function () {
+    // this.aside();
+  },
+  
+};
+
+var Common = {
+  init: function () {
+    this.common();
+  },
+
+  common: function () {
+    // confirm 모달
+  },
+};
+
+Header.init();
+Common.init();
